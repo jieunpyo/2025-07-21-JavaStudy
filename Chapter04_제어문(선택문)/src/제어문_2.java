8ecf3577efd5492b7aa53942e1fcd904f9b87330
jieunpyo <jieunhye1121@naver.com>
2025-07-28 15:37:06 +0900

    1111

---
+++ Chapter04_제어문(선택문)/src/제어문_2.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+/*
+ * 	 1. 사용자 입력
+ * 	 2. 입력 처리
+ * 		-------
+ * 	 3. 결과값 출력 
+ */
+public class 제어문_2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scan=new Scanner(System.in);
+		
+	}
+
+}
