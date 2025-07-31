08d58c310e84016ccfe9cb1a0af91898e2005cb8
jieunpyo <jieunhye1121@naver.com>
2025-07-31 16:01:25 +0900

    1111

---
+++ Chapter04_제어문_응용/src/문자열_1.java
@@ -0,0 +1,50 @@
+/*
+ * 	 문자열
+ * 	 ----- 자바
+ * 	 String => 데이터형 / 클래스형 (기능=>메소드)
+ * 	 => 사용법
+ * 		String 변수명="문자"
+ * 					 ----- 제한이 없다
+ * 		예) 회원 정보 
+ * 			------- id,pwd,name,email,address,phone,post
+ * 					content ..... birthday ...
+ * 			게시판 정보
+ * 			  no name subject content pwd regdate hit
+ * 		--> 제공하는 기능
+ * 			메소드
+ * 		변환
+ * 		 toUpperCase() : 대문자 변환
+ * 		 toLowerCase() : 소문자 변환
+ * 		 ****** valueOf() : 모든 데이터형을 문자열로 변환
+ * 		 		웹 / 윈도우 => 모든 데이터형 String
+ * 				valueOf(10) => "10"
+ * 		 -------------------------(사용빈도가 없다) 한글중심 
+ * 		비교
+ * 		 equels() => 실제 문자열 비교
+ * 		 contains() => 포함 문자열 
+ * 		 startsWith() => 시작문자열
+ * 		 endsWith() => 끝문자열 => 확장 
+ * 		검색
+ * 		 Hello Java!!
+ * 		 0123456789
+ *		 int indexOf("a") => 7
+ *		 int indexOf("Java") => 6
+ *		 => 앞
+ *		 lastIndexOf("a") => 9
+ *		 => 뒤
+ *
+ *		[{},{},{},{},....]
+ * 		제어
+ * 		 substring(1,5) : 문자를 자르기
+ * 					end-1
+ * 	 	 split() => 문자별 자르기
+ * 		 trim() => 공백문자 제거 
+ */
+public class 문자열_1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
