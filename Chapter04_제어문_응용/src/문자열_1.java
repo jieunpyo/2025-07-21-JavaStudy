96f8c23258eac5760183ffbfbff0c4407e438606
jieunpyo <jieunhye1121@naver.com>
2025-07-31 17:51:23 +0900

    1111

--- Chapter04_제어문_응용/src/문자열_1.java
+++ Chapter04_제어문_응용/src/문자열_1.java
@@ -39,12 +39,75 @@
  * 					end-1
  * 	 	 split() => 문자별 자르기
  * 		 trim() => 공백문자 제거 
+ * 	  --------------------------------
+ * 	  
+ * 	  
  */
-public class 문자열_1 {
+// substring
+import java.io.*;
+import java.util.*;
+public class 문자열_1{
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception{
 		// TODO Auto-generated method stub
-
+		String msg="Hello Java";
+		//1. 문자의 갯수 => " " => 문자
+		// => length() -> int
+		int leng=msg.length();
+		System.out.println("문자 갯수:"+leng);
+		//2. 자르기
+		String s=msg.substring(6);
+		System.out.println(s);
+		s=msg.substring(1, 5);
+		// substring(beingIndex, endIndex);
+		//						 --------- endIndex-1
+		System.out.println(s);
+		String name="문자열_1.java";
+		//			 0 12345
+		String ext=name.substring(name.indexOf(".")+1);
+		
+		/*File dir=new File("c:\\javaDev");
+		File[] lists=dir.listFiles();
+		// ./  ..
+		for(File f:lists)
+		{
+			if(f.isDirectory())//isFile
+			{
+				String ss=f.getName();
+				System.out.println(ss);
+				//System.out.println(ss.substring(ss.lastIndexOf(".")+1));
+			}
+		}*/
+		
+		String msg1="                  Hello Java!! ";
+		System.out.println(msg1.length());
+		String ss=msg1.trim();
+		System.out.println(ss.length());
+		// trim() => 공백문자 제거 => 좌우
+		// trim() => 어디서 => 검색 / 로그인 / 아이디중복체크
+		// 액션 | ㅁㅁㅁ | ㅁㅁㅁㅁ
+		// 서울시 마포구 서교동
+		// => 지역 확인 02-1111-1111
+		String msg2="Java Oracle HTML CSS JavaScript "
+				   +"JSP JQuery Ajax SpringFramework VueJS "
+				   +"SpringBoot Git GitAction Docker Docker-Compose "
+				   +"Jenkins React Next TypeScript MSA Chatting NodeJS";
+		String[] datas=msg2.split(" ");
+		Scanner scan=new Scanner(System.in);
+		System.out.print("단어 입력:");
+		String d=scan.next();
+		for(String data:datas)
+		{
+			//System.out.println(data);
+			// data.startsWith(d)
+			// data.endsWith(d)
+			//data.contains(d)
+			// data.equals(d) ===> 대소문자 구분
+			if(data.equalsIgnoreCase(d))
+			{
+				System.out.println(data);
+			}
+		}
 	}
 
 }
