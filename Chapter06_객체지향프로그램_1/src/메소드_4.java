b5b95bf7cefdbfd9d3d054fab5c7734948fb6def
jieunpyo <jieunhye1121@naver.com>
2025-08-06 14:28:15 +0900

    1111

---
+++ Chapter06_객체지향프로그램_1/src/메소드_4.java
@@ -0,0 +1,19 @@
+/*
+ * 		  
+ * 		  리턴형 메소드명(매개변수...) => 선언부 
+ * 		  {
+ * 			  구현부 
+ * 		  } 
+ * 		  => 파이썬 return 10,20,30...
+ * 		  -----------
+ * 		   
+ * 		  -----------
+ */
+public class 메소드_4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
