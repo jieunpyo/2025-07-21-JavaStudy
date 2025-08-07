16f14416251bf8870ba8ff97580f3b5f59cc816d
jieunpyo <jieunhye1121@naver.com>
2025-08-07 15:59:07 +0900

    1111

---
+++ Chapter06_객체지향(메소드)_1/src/Member.java
@@ -0,0 +1,40 @@
+
+public class Member {
+	private String id;
+	private String pwd;
+	private String name;
+	private String sex;
+	private int age;
+	
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getPwd() {
+		return pwd;
+	}
+	public void setPwd(String pwd) {
+		this.pwd = pwd;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getSex() {
+		return sex;
+	}
+	public void setSex(String sex) {
+		this.sex = sex;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	
+}
