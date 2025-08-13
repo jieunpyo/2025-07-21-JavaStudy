5d413aa9488f41fb5ad8656f7a54edb8725a97f4
jieunpyo <jieunhye1121@naver.com>
2025-08-13 14:00:32 +0900

    1111

---
+++ Chapter07_객체지향프로그램(재사용)/src/com/sist/main/재사용_2.java
@@ -0,0 +1,48 @@
+package com.sist.main;
+/*
+ * 	 class A
+ * 	 {
+ * 		int a;
+ * 		int b;
+ * 		public void aaa(){}
+ * 	 }
+ * 	 class B extends A
+ * 	 {
+ * 		int c;
+ * 		public void bbb(){}
+ * 
+ * 		//int a;
+ * 		//int b;
+ * 		//public void aaa(){}
+ * 	 }
+ * 
+ * 	 B bb=new B();
+ * 		스택
+ * 	 ---bb----
+ * 	 저장된 주소
+ * 	 ---------	 ----------------
+ * 				   ---super---
+ * 					----a-----
+ * 					  100
+ * 					----b-----
+ * 					  이순신
+ * 					---------
+ *					 aaa()
+ *					---------
+ *				   -----------
+ *
+ *				  -----this-------
+ *					-----c-------
+ *
+ *				   -------------
+ *					  bbb()
+ * 				 ----------------
+ */
+public class 재사용_2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
