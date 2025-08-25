76b92cb6ea026eec189d2cbcf4fdd1d55bb3071d
jieunpyo <jieunhye1121@naver.com>
2025-08-25 13:49:48 +0900

    1111

---
+++ Chapter15_13_컬렉션프레임워크/src/com/sist/list/Vector_2.java
@@ -0,0 +1,13 @@
+package com.sist.list;
+import java.net.*;
+public class Vector_2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        try
+        {
+        	Socket s=new Socket("localhost",3355);
+        }catch(Exception ex) {}
+	}
+
+}
