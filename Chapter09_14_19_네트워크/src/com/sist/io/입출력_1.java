078d3c060fe5c01181842d084d3930026d5f6b76
jieunpyo <jieunhye1121@naver.com>
2025-08-26 16:53:51 +0900

    1111

---
+++ Chapter09_14_19_네트워크/src/com/sist/io/입출력_1.java
@@ -0,0 +1,19 @@
+package com.sist.io;
+/*
+ * 	 웹 
+ * 	  = 컬렉션 / 사용자 정의 데이터형 => ~VO
+ * 	  = 입출력 => IO
+ * 	  = 데이터베이스
+ * 	  --------------------------------
+ * 	   브라우저에 전송 => HTML (J2EE)
+ * 	   
+ * 
+ */
+public class 입출력_1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
